2f12a41479374f371d97c47aba340885f0575cee
Vladimir <vladimir.stojoc@iis.utm.md>
2023-02-05 16:48:31 +0200

    setup.py change

--- setup.py
+++ setup.py
@@ -1,38 +1,69 @@
 from setuptools import setup
 long_description = '''
-Reparo is a python sci-kit learn inspired package for Missing Value Imputation. It contains a some feature transformers to eliminate Missing Values (NaNs) from your data for Machine Learning Algorithms.\n
-This version of reparo has the next methods of missing value imputation:\n
-1) Cold-Deck Imputation (CDI).\n
-2) Hot-Deck Imputation (HotDeckImputation).\n
-3) Fuzzy-Rough Nearest Neighbor for Imputation (FRNNI).\n
-4) K-Nearest Neighbors Imputation (KNNI).\n
-5) Single Center Imputation from Multiple Chained Equation (SICE).\n
-6) Predictive Mean Matching (PMM).\n
-7) Multivariate Imputation by Chained Equation (MICE).\n
-All these methods work like normal sklearn transformers. They have fit, transform and fit_transform functions implemented.\n
-Additionally every reparo transformer has an apply function which allows to apply an transformation on a pandas Data Frame.\n
-How to use reparo\n
-To use a transformer from reparo you should just import the transformer from reparo in the following framework:\n
-```from reparo import <class name>```\n
-class names are written above in parantheses.\n
-Next create a object of this algorithm (I will use k-Nearest Neighbors Imputation as an example).\n
-```method = KNNI()```\n
-Firstly you should fit the transformer, passing to it a feature matrix (X) and the target array (y). y argument is not really used (as it causes data leackage)\n
-```method.fit(X, y)```\n
-After you fit the model, you can use it for transforming new data, using the transform function. To transform function you should pass only the feature matrix (X).\n
-```X_transformed = method.transform(X)```\n
-Also you can fit and transform the data at the same time using the fit_transform function.\n
-```X_transformed = method.fit_transform(X)```\n
-Also you can apply a transformation directly on a pandas DataFrame, choosing the columns that you want to change.\n
-```new_df = method.apply(df, 'target', ['col1', 'col2'])```\n
-With love from Sigmoid.\n
-We are open for feedback. Please send your impression to papaluta.vasile@isa.utm.md\n
+# reparo
+
+Reparo is a python sci-kit learn inspired package for Missing Value Imputation. It contains a some feature transformers to eliminate Missing Values (NaNs) from your data for Machine Learning Algorithms.
+
+This version of reparo has the next methods of missing value imputation:
+1) Cold-Deck Imputation (CDI).
+2) Hot-Deck Imputation (HotDeckImputation).
+3) Fuzzy-Rough Nearest Neighbor for Imputation (FRNNI).
+4) K-Nearest Neighbors Imputation (KNNI).
+5) Single Center Imputation from Multiple Chained Equation (SICE).
+6) Predictive Mean Matching (PMM).
+7) Multivariate Imputation by Chained Equation (MICE).
+
+All these methods work like normal sklearn transformers. They have fit, transform and fit_transform functions implemented.
+
+Additionally every reparo transformer has an apply function which allows to apply an transformation on a pandas Data Frame.
+
+# How to use reparo
+To use a transformer from reparo you should just import the transformer from reparo in the following framework:
+
+```python
+from reparo import MICE
+```
+
+class names are written above in parantheses.
+
+Next create a object of this algorithm (I will use k-Nearest Neighbors Imputation as an example).
+
+```python
+method = KNNI()
+```
+
+Firstly you should fit the transformer, passing to it a feature matrix (X) and the target array (y). y argument is not really used (as it causes data leackage)
+
+```python
+method.fit(X, y)
+```
+
+After you fit the model, you can use it for transforming new data, using the transform function. To transform function you should pass only the feature matrix (X).
+
+```python
+X_transformed = method.transform(X)
+```
+
+Also you can fit and transform the data at the same time using the fit_transform function.
+
+```python
+X_transformed = method.fit_transform(X)
+```
+
+Also you can apply a transformation directly on a pandas DataFrame, choosing the columns that you want to change.
+
+```python
+new_df = method.apply(df, 'target', ['col1', 'col2'])
+```
+
+With <3 from Sigmoid.
+We are open for feedback. Please send your impression to vladimir.stojoc@gmail.com
 '''
 
 setup(
   name = 'reparo',
   packages = ['reparo'],
-  version = '0.0.5',
+  version = '0.0.6',
   license='MIT',
   description = 'Reparo is a python sci-kit learn inspired package for Missing Value Imputation.',
   long_description=long_description,
@@ -66,5 +97,5 @@ setup(
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
   ],
-    long_description_content_type='text/x-rst',
+  long_description_content_type='text/markdown',
 )
